c0b20f818486cfff63c29cc57412b5e9b2aa76ef
adeyemi <mathewadeyemi7654@gmail.com>
2024-10-03 19:20:42 +0100

    remove nodemon

--- api/server.js
+++ api/server.js
@@ -1,8 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-
-import { connectDB } from "./connectDb.js";
+import mongoose from "mongoose";
 import { jobRoute } from "./routes/jobs.js";
 const app = express();
 dotenv.config();
@@ -10,6 +9,18 @@ app.use(cors());
 app.use(express.json());
 
 const port = process.env.port || 4000;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useUnifiedTopology: true,
+    });
+    console.log(`mongodb database connected successfully.`);
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
 connectDB();
 app.use("/api/jobs", jobRoute);
 app.get("/", (req, res) => res.send("welcome to jobs app"));
